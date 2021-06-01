abbf8020c3e5dc2cb12d1d1b3547a18ef1830c68
Palani Ram <palani.ram@ihsmarkit.com>
2021-06-01 02:26:29 -0400

    add  functions

---
+++ python/hello_world.py
@@ -0,0 +1,17 @@
+
+def helloGET(request):
+    """Responds to any HTTP request.
+    Args:
+        request (flask.Request): HTTP request object.
+    Returns:
+        The response text or any set of values that can be turned into a
+        Response object using
+        `make_response <https://flask.palletsprojects.com/en/2.0.x/api/>`.
+    """
+    request_json = request.get_json()
+    if request.args and 'message' in request.args:
+        return request.args.get('message')
+    elif request_json and 'message' in request_json:
+        return request_json['message']
+    else:
+        return f'Hello World!'
