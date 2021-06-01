4be61ff21df7f950bf6f3cc5698a620afa9c88d4
Palani Ram <palani.ram@ihsmarkit.com>
2021-06-01 02:26:29 -0400

    add  functions

---
+++ modules/function/variables.tf
@@ -0,0 +1,29 @@
+
+variable "project_id" {
+  type = string
+}
+variable "region" {
+  type = string
+}
+variable "function_name" {
+  type = string
+}
+variable "description" {
+  type = string
+}
+variable "bucket_name" {
+  type = string
+}
+variable "zip_file_path" {
+  type = string
+}
+variable "zip_file_name" {
+  type = string
+}
+variable "source_code_folder" {
+  type = string
+}
+
+
+
+
