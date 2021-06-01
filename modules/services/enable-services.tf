eb68970b6a0184d89112a648caaaf3db5e4c42db
Palani Ram <palani.ram@ihsmarkit.com>
2021-06-01 11:00:13 -0400

    updating project services with github url

--- modules/services/enable-services.tf
+++ modules/services/enable-services.tf
@@ -1,9 +1,10 @@
 module "project-services" {
   // https://registry.terraform.io/modules/terraform-google-modules/project-factory/google/latest/submodules/project_services
-  source     = "terraform-google-modules/project-factory/google//modules/project_services"
-  version    = "10.3.2"
-  project_id = var.project_id
-
+  source                      = "git::https://github.com/terraform-google-modules/terraform-google-project-factory.git//modules/project_services?ref=master"
+  project_id                  = var.project_id
+  enable_apis                 = true
+  disable_dependent_services  = true
+  disable_services_on_destroy = false
   activate_apis = [
     "vpcaccess.googleapis.com", // Serverless VPC Access API
     "compute.googleapis.com",
