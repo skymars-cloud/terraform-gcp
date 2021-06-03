b8c7c3d8e4494f02fab7f654f2b5f7d5e6443051
Palani Ram <palani.ram@ihsmarkit.com>
2021-06-03 03:28:58 -0400

    python function created and deployed using terraform

--- base-infra/modules/services/enable-services.tf
+++ base-infra/modules/services/enable-services.tf
@@ -1,3 +1,7 @@
+
+// Project API Activation - this module is not recommended. enable service feature will be removed in future. so use the commands in shell script.
+// https://github.com/terraform-google-modules/terraform-google-project-factory/tree/master/modules/project_services#project-api-activation
+// this does not enable the services even though it says created (enabled) but tf destroy removes it. DO NOT USE THIS MODULE
 module "project-services" {
   // https://registry.terraform.io/modules/terraform-google-modules/project-factory/google/latest/submodules/project_services
   source                      = "git::https://github.com/terraform-google-modules/terraform-google-project-factory.git//modules/project_services?ref=master"
@@ -15,7 +19,20 @@ module "project-services" {
     "gkehub.googleapis.com",
     "cloudfunctions.googleapis.com",
     "logging.googleapis.com",
-    "cloudresourcemanager.googleapis.com"
+    "cloudresourcemanager.googleapis.com",
+    "cloudbuild.googleapis.com" //  Cloud Build API - Continuously build, test, and deploy.
 
   ]
 }
+//all the above resources are removed from the tf state file
+//tf state rm 'module.dev_services.module.project-services.google_project_service.project_services["cloudbuild.googleapis.com"]'
+//tf state rm 'module.dev_services.module.project-services.google_project_service.project_services["cloudfunctions.googleapis.com"]'
+//tf state rm 'module.dev_services.module.project-services.google_project_service.project_services["cloudresourcemanager.googleapis.com"]'
+//tf state rm 'module.dev_services.module.project-services.google_project_service.project_services["compute.googleapis.com"]'
+//tf state rm 'module.dev_services.module.project-services.google_project_service.project_services["container.googleapis.com"]'
+//tf state rm 'module.dev_services.module.project-services.google_project_service.project_services["containeranalysis.googleapis.com"]'
+//tf state rm 'module.dev_services.module.project-services.google_project_service.project_services["gkehub.googleapis.com"]'
+//tf state rm 'module.dev_services.module.project-services.google_project_service.project_services["iam.googleapis.com"]'
+//tf state rm 'module.dev_services.module.project-services.google_project_service.project_services["logging.googleapis.com"]'
+//tf state rm 'module.dev_services.module.project-services.google_project_service.project_services["serviceusage.googleapis.com"]'
+//tf state rm 'module.dev_services.module.project-services.google_project_service.project_services["vpcaccess.googleapis.com"]'
