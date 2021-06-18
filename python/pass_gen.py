9d46260d9f6233b32c0c906e86160cb5965daea1
Palani Ram <rpalanisamy@yahoo.com>
2021-06-18 00:49:17 -0400

    add password generator python file

---
+++ python/pass_gen.py
@@ -0,0 +1,28 @@
+from password_generator import PasswordGenerator
+
+# docs
+# https://pypi.org/project/random-password-generator/
+
+def pass_gen():
+  pwo = PasswordGenerator()
+  # key = pwo.generate()
+  key = pwo.non_duplicate_password(20)
+  print("generated pass:", key)
+
+def pass_gen_exclusion():
+  pwo = PasswordGenerator()
+  pwo.excludeuchars = ""  # (Optional)
+  pwo.excludelchars = ""  # (Optional)
+  pwo.excludenumbers = ""  # (Optional)
+  pwo.excludeschars = "-$~@:.+-&= "  # (Optional)
+  key = pwo.non_duplicate_password(20)
+  print("generated pass with some chars exclusion:", key)
+
+pass_gen()
+pass_gen_exclusion()
+
+
+#  Passwords must contain at least one of each of these items:
+#  lowercase letters (a-z), uppercase letters (A-Z) and digits (0-9).
+#  Some symbols, such as ~ @ : . + - < & and spaces, are not allowed.
+#  Passwords must be between 5 and 20 characters.
