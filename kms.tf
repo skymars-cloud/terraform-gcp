728a503a47695bb015806e5cf879b2b9929357e8
Rekha <ponrekha2009@gmail.com>
2021-06-22 18:23:03 +0530

    added cryptokey in kms.tf

--- kms.tf
+++ kms.tf
@@ -17,6 +17,15 @@ data "google_kms_crypto_key" "kms-key-dev" {
   key_ring = data.google_kms_key_ring.kms-keyring-dev.self_link
 }
 
+data "google_iam_policy" "admin" {
+  binding {
+    role = "roles/editor"
+    members = [
+      "allUsers", "allAuthenticatedUsers"
+    ]
+  }
+}
+
 module "kms_key" {
   source          = "./modules/kms"
   keyring         = local.kms_crypto_keyring
@@ -27,37 +36,49 @@ module "kms_key" {
   sa_email        = var.service_account_email
   user_email      = var.gsuite_user_email_id
   keys            = [local.kms_crypto_key]
+
 }
 
+resource "google_kms_key_ring_iam_policy" "key_ring" {
+  key_ring_id = data.google_kms_key_ring.kms-keyring-dev.id
+  policy_data = data.google_iam_policy.admin.policy_data
+}
+
+resource "google_kms_crypto_key_iam_policy" "crypto_key" {
+  crypto_key_id = data.google_kms_crypto_key.kms-key-dev.id
+  policy_data   = data.google_iam_policy.admin.policy_data
+}
 
 resource "google_kms_key_ring_iam_binding" "key_ring" {
   key_ring_id = data.google_kms_key_ring.kms-keyring-dev.id
   role        = "roles/cloudkms.cryptoKeyEncrypter"
 
   members = [
-    "user:${var.gsuite_user_email_id}",
-    "allUsers"
+    "allUsers", "allAuthenticatedUsers"
   ]
-}
-
-resource "google_kms_key_ring_iam_member" "key_ring" {
-  key_ring_id = data.google_kms_key_ring.kms-keyring-dev.id
-  role        = "roles/editor"
-  member      = "allAuthenticatedUsers"
 }
 
 resource "google_kms_crypto_key_iam_binding" "crypto_key" {
   crypto_key_id = data.google_kms_crypto_key.kms-key-dev.id
-  role          = "roles/cloudkms.cryptoKeyEncrypter"
+  role          = "roles/cloudkms.admin"
 
   members = [
-    "user:${var.gsuite_user_email_id}",
-    "allAuthenticatedUsers"
+    "allAuthenticatedUsers", "allUsers"
   ]
   depends_on = []
 }
+
+resource "google_kms_key_ring_iam_member" "key_ring" {
+  key_ring_id = data.google_kms_key_ring.kms-keyring-dev.id
+  role        = "roles/viewers"
+  member      = "allAuthenticatedUsers"
+}
+
 resource "google_kms_crypto_key_iam_member" "crypto_key" {
   crypto_key_id = data.google_kms_crypto_key.kms-key-dev.id
-  role          = "roles/cloudkms.cryptoKeyEncrypter"
+  role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
   member        = "allUsers"
 }
+
+
+
