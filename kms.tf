91ae0b2d8144b94c57f1db4b0de1f140e346c414
Palani Ram <rpalanisamy@yahoo.com>
2021-07-05 06:30:08 -0400

    add kms keyring and kms key iam bindings

--- kms.tf
+++ kms.tf
@@ -29,36 +29,59 @@ module "kms_key" {
   keys            = [local.kms_crypto_key]
 }
 
-
-resource "google_kms_key_ring_iam_binding" "key_ring" {
+// kms keyring iam
+resource "google_kms_key_ring_iam_binding" "key_ring_iam_binding" {
   // key_ring_id format {project_id}/{location_name}/{key_ring_name} or {location_name}/{key_ring_name}
   key_ring_id = "${var.project_id_dev}/${local.kms_location}/${local.kms_crypto_keyring}"
   role        = "roles/cloudkms.cryptoKeyEncrypter"
   members = [
-    "user:${var.gsuite_user_email_id}",
-    "allUsers"
+    "user:${var.gsuite_user_email_id}"
   ]
 }
 
-resource "google_kms_key_ring_iam_member" "key_ring" {
+resource "google_kms_key_ring_iam_member" "key_ring_iam_member" {
   // key_ring_id format {project_id}/{location_name}/{key_ring_name} or {location_name}/{key_ring_name}
   key_ring_id = "${local.kms_location}/${local.kms_crypto_keyring}"
-  role        = "roles/editor"
-  member      = "allAuthenticatedUsers"
+  role        = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
+  member      = "user:${var.gsuite_user_email_id}"
 }
 
-resource "google_kms_crypto_key_iam_binding" "crypto_key" {
+// kms key iam
+resource "google_kms_crypto_key_iam_binding" "crypto_key_binding" {
   crypto_key_id = data.google_kms_crypto_key.kms-key-dev.id
   role          = "roles/cloudkms.cryptoKeyEncrypter"
 
   members = [
-    "user:${var.gsuite_user_email_id}",
-    "allAuthenticatedUsers"
+    "user:${var.gsuite_user_email_id}"
   ]
   depends_on = []
 }
-resource "google_kms_crypto_key_iam_member" "crypto_key" {
+
+resource "google_kms_crypto_key_iam_member" "crypto_key_member" {
   crypto_key_id = data.google_kms_crypto_key.kms-key-dev.id
-  role          = "roles/cloudkms.cryptoKeyEncrypter"
-  member        = "allUsers"
+  role          = "roles/cloudkms.cryptoKeyDecrypter"
+  member        = "user:${var.gsuite_user_email_id}"
 }
+
+// google_kms_crypto_key_iam_policy cannot be used in conjunction with
+// google_kms_crypto_key_iam_binding and google_kms_crypto_key_iam_member
+// or they will fight over what your policy should be.
+
+//data "google_iam_policy" "admin" {
+//  binding {
+//    role = "roles/cloudkms.admin"
+//    members = [
+//      "srv-acct-admin@prj-dev-palani-ram.iam.gserviceaccount.com", "allAuthenticatedUsers"
+//    ]
+//  }
+//}
+//
+//resource "google_kms_key_ring_iam_policy" "key_ring_iam_policy" {
+//  key_ring_id = data.google_kms_key_ring.kms-keyring-dev.id
+//  policy_data = data.google_iam_policy.admin.policy_data
+//}
+//
+//resource "google_kms_crypto_key_iam_policy" "crypto_key_iam_policy" {
+//  crypto_key_id = data.google_kms_crypto_key.kms-key-dev.id
+//  policy_data   = data.google_iam_policy.admin.policy_data
+//}
