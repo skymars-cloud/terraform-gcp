0ecae85085c3cdd6009d6991d0905ba797d61603
Palani Ram <rpalanisamy@yahoo.com>
2021-07-13 18:57:10 -0400

    dns sec sec terraform added

--- modules/dns/cloud-dns.tf
+++ modules/dns/cloud-dns.tf
@@ -11,17 +11,17 @@ resource "google_dns_managed_zone" "zone1" {
 
   dnssec_config {
     //        state = "on"
-    state = "off"
-    //    kind          = "dns#managedZoneDnsSecConfig"
-    //    non_existence = "nsec3"
-    //    default_key_specs {
-    //      key_type = "keySigning"
-    //      //      key_type = "zoneSigning"     //      ZONE_SIGNING / RSASHA1 / 1024
-    //      key_length = 1024
-    //      kind       = "dns#dnsKeySpec"
-    //      algorithm = "rsasha1"
-    //      //      ecdsap256sha256 ecdsap384sha384 rsasha1 rsasha256 rsasha512   //algorithm allowed values
-    //    }
+    state         = "off"
+    kind          = "dns#managedZoneDnsSecConfig"
+    non_existence = "nsec3"
+    default_key_specs {
+      //      key_type = "keySigning"
+      key_type   = "zoneSigning" //      ZONE_SIGNING / RSASHA1 / 1024
+      key_length = 1024
+      kind       = "dns#dnsKeySpec"
+      algorithm  = "rsasha1"
+      //      ecdsap256sha256 ecdsap384sha384 rsasha1 rsasha256 rsasha512   //algorithm allowed values
+    }
   }
 
 }
