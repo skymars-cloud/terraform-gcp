c590fa68ea7d422a5a605b31361bf56f0d3c424e
Palani Ram <rpalanisamy@yahoo.com>
2021-07-31 02:43:41 -0400

    add random string and id to tf output

--- _outputs.tf
+++ _outputs.tf
@@ -1,18 +1,18 @@
-//
-//output "random_string" {
-//  value       = random_string.random.result
-//  description = "random_string generated"
-//}
-//output "random_id_b64_std" {
-//  value       = random_id.server.b64_std
-//  description = "random_id generated"
-//}
-//output "random_id_b64_url" {
-//  value       = random_id.server.b64_url
-//  description = "random_id generated"
-//}
-//output "random_shuffle" {
-//  value       = random_shuffle.az.result
-//  description = "random_shuffle generated"
-//}
-//
+
+output "random_string" {
+  value       = random_string.random.result
+  description = "random_string generated"
+}
+output "random_id_b64_std" {
+  value       = random_id.server.b64_std
+  description = "random_id generated"
+}
+output "random_id_b64_url" {
+  value       = random_id.server.b64_url
+  description = "random_id generated"
+}
+output "random_shuffle" {
+  value       = random_shuffle.az.result
+  description = "random_shuffle generated"
+}
+
